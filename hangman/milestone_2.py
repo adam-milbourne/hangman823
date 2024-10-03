17a9e03bdf3d6aed0262c72f5aa9f03a299dce41
Adam MILBOURNE <milbourne.adam@yahoo.com>
2024-10-03 15:08:23 +0100

    Removes previous player guess function

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,20 +1,23 @@
 import random #imports random module
 
 #VARIABLES
-word_list = ["Apple", "Banana", "Grapes", "Strawberries", "Pineapples"] #list of fruits to choose word from
-print(word_list)#check list prints
+word_list = ["apple", "banana", "grapes", "strawberries", "pineapples"] #list of fruits to choose word from
+print(word_list)#check list prints (TODO: make sure to remove this once debugging is over otherwise will show player the words list)
 
 word = random.choice(word_list)#creates a variable called work from the list
 
-print(word)#check it chooses a random word every time
+print(word)#check it chooses a random word every time (TODO: make sure to remove this once debugging is over otherwise will show player the word)
 
 
-def player_guess_letter():
-    guess = input("Guess a letter: ")
-    if len(guess) == 1 & guess.isalpha() == True:
-        print("Good guess!")  
-    else:
-     print("Oops! That is not a valid input")
 
-player_guess_letter()
+
+#!!!!!!!!!RECREATED IN MILESTONE 2!!!!!!!!!!!!!!!
+#def player_guess_letter():
+#    guess = input("Guess a letter: ")
+#    if len(guess) == 1 & guess.isalpha() == True:
+#        print("Good guess!")  
+#    else:
+#     print("Oops! That is not a valid input")
+
+#player_guess_letter()
 
